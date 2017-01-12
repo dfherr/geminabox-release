418c41d51a6332c3ecaa7eaacf0f9a0a438ed37f
Dennis Herr <dennis.herr@experteer.com>
2017-01-12 16:59:02 +0100

    Version 1.0.0

--- lib/geminabox-release/version.rb
+++ lib/geminabox-release/version.rb
@@ -1,6 +1,8 @@
 module GeminaboxRelease
+        # Version 1.0.0
+        # bundler >= 1.0.14 actually is a dependency
 	# Version 0.2.0
 	# * @hilli added support for https with optional veriy_none mode
-	VERSION = "0.2.0"
+	VERSION = "1.0.0"
 end
 
