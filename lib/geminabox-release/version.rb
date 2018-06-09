cebc38582980ec4dec2fc8f92748278e7e8d2368
dherr <dherr@hubspot.com>
2018-06-09 17:55:11 +0100

    Version 1.2.0

--- lib/geminabox-release/version.rb
+++ lib/geminabox-release/version.rb
@@ -1,8 +1,15 @@
 module GeminaboxRelease
-        # Version 1.0.0
-        # bundler >= 1.0.14 actually is a dependency
+	# Version 1.2.0
+	# * exit with non-zero code on unsuccessful upload but successful http call
+	#
+	# Version 1.1.0
+	# * Improved support for basic authentication
+	#
+  # Version 1.0.0
+  # * bundler >= 1.0.14 actually is a dependency
+	#
 	# Version 0.2.0
 	# * @hilli added support for https with optional veriy_none mode
-	VERSION = "1.1.0"
+	#
+	VERSION = "1.2.0"
 end
-
